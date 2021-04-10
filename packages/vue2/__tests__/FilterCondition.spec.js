be6072302f5fd40996b8f9b0b3ed4ff3dd28b8a7
Obada Khalili <obada.akhalili@gmail.com>
2021-04-10 11:04:17 +0300

    vue2: write first unit test of FilterCondition.vue

--- packages/vue2/__tests__/FilterCondition.spec.js
+++ packages/vue2/__tests__/FilterCondition.spec.js
@@ -1,7 +1,29 @@
-const FilterCondition = require("../src/VueVisualFilter/FilterCondition.vue")
+import "regenerator-runtime"
+import { mount } from "@vue/test-utils"
+
+import FilterCondition from "../src/VueVisualFilter/FilterCondition.vue"
 
 describe("FilterCondition.vue", () => {
-  it("should pass", () => {
-    console.log(FilterCondition)
+  const wrapper = mount(FilterCondition, {
+    propsData: {
+      condition: {
+        argument: "Obada",
+        dataType: "nominal",
+        fieldName: "First Name",
+        method: "contains",
+        type: "condition",
+      },
+      fieldNames: ["First Name", "Last Name", "Grade"],
+      nominalMethodNames: ["contains", "startsWith", "endsWith"],
+      numericMethodNames: ["=", ">", "<"],
+    },
+  })
+
+  it("emits event on delete button click", async () => {
+    const button = wrapper.find("button")
+
+    await button.trigger("click")
+
+    expect(wrapper.emitted()).toHaveProperty("deleteCondition")
   })
 })
